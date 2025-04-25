590ca8a5108c764937e5620390072454b3c6655c
mayank-gajengi22 <mayank2498m@gmail.com>
2025-04-25 07:03:50 +0530

    Update 23userdefexcep.py

--- 23userdefexcep.py
+++ 23userdefexcep.py
@@ -1,23 +1,12 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# User-defined exception
+class PasswordError(Exception):
+    pass
 
-class Student(Person):
-    def __init__(self, name, age, roll_no):
-        super().__init__(name, age)
-        self.roll_no = roll_no
-
-class CollegeStudent(Student):
-    def __init__(self, name, age, roll_no, course):
-        super().__init__(name, age, roll_no)
-        self.course = course
-    
-    def display_info(self):
-        print("Name:", self.name)
-        print("Age:", self.age)
-        print("Roll No:", self.roll_no)
-        print("Course:", self.course)
-
-student = CollegeStudent("Bob", 21, "B567", "Electrical Engineering")
-student.display_info()
+# Main program
+try:
+    password = input("Enter your password: ")
+    if password != "hello123":
+        raise PasswordError
+    print("Password is correct!")
+except PasswordError:
+    print("Wrong password!")
